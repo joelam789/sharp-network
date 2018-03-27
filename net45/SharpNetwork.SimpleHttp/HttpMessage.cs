17726d23444ae397c8f4b4f4b1d0a0c073436ec3
joelam789 <joelam789@yahoo.com.hk>
2018-03-27 16:25:49 +0800

    add SharpNetwork.SimpleHttp

---
+++ net45/SharpNetwork.SimpleHttp/HttpMessage.cs
@@ -0,0 +1,377 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization.Json;
+using System.Text;
+using System.Threading.Tasks;
+
+using SharpNetwork.Core;
+
+namespace SharpNetwork.SimpleHttp
+{
+    public class HttpMessage
+    {
+        public const int WEB_MSG_BUF_CODE = -1;
+        public const int WEB_MSG_DATA_CODE = -2;
+        public const int WEB_MSG_TASK_CODE = -3;
+
+        public const int STATE_WAIT_FOR_HEADER = 0;
+        public const int STATE_WAIT_FOR_BODY = 1;
+        public const int STATE_READY = 2;
+
+        public const int MSG_TYPE_UNDEFINED = 0;
+        public const int MSG_TYPE_HANDSHAKE = 1;
+        public const int MSG_TYPE_STRING = 2;
+        public const int MSG_TYPE_BINARY = 3;
+
+        public enum HttpStatusCode
+        {
+            // for a full list of status codes, see..
+            // https://en.wikipedia.org/wiki/List_of_HTTP_status_codes
+
+            Continue = 100,
+
+            Ok = 200,
+            Created = 201,
+            Accepted = 202,
+            MovedPermanently = 301,
+            Found = 302,
+            NotModified = 304,
+            BadRequest = 400,
+            Forbidden = 403,
+            NotFound = 404,
+            MethodNotAllowed = 405,
+            InternalServerError = 500
+        }
+
+        private static ConcurrentExclusiveSchedulerPair m_TaskSchedulerPair
+            = new ConcurrentExclusiveSchedulerPair();
+
+        public int ReceivingState { get; set; }
+
+        public int ContentSize { get; set; }
+        public int MessageType { get; set; }
+
+        public Dictionary<string, string> Headers { get; set; }
+
+        //public Uri Url { get; set; }
+
+        public string RequestUrl { get; set; }
+        public string RequestMethod { get; set; }
+
+        public int StatusCode { get; set; }
+        public string ReasonPhrase { get; set; }
+
+        public string ProtocolVersion { get; set; }
+
+        public string MessageContent { get; set; }
+        public byte[] RawContent { get; set; }
+
+        private IJsonCodec m_JsonCodec = null;
+        private static IJsonCodec m_CurrentJsonCodec = null;
+        private static IJsonCodec m_DefaultJsonCodec = new SimpleJsonCodec();
+        public static IJsonCodec DefaultJsonCodec
+        {
+            get
+            {
+                return m_DefaultJsonCodec;
+            }
+        }
+        public static IJsonCodec JsonCodec
+        {
+            get
+            {
+                return m_CurrentJsonCodec == null ? m_DefaultJsonCodec : m_CurrentJsonCodec;
+            }
+            set
+            {
+                m_CurrentJsonCodec = value;
+            }
+        }
+
+        public HttpMessage()
+        {
+            ReceivingState = STATE_WAIT_FOR_HEADER;
+
+            ContentSize = 0;
+            MessageType = 0;
+
+            StatusCode = 200;
+            ReasonPhrase = "OK";
+
+            ProtocolVersion = "";
+
+            RequestUrl = "/";
+            RequestMethod = "";
+
+            MessageContent = "";
+            RawContent = null;
+
+            //Url = null;
+
+            Headers = new Dictionary<string, string>();
+
+            m_JsonCodec = m_CurrentJsonCodec;
+            if (m_JsonCodec == null) m_JsonCodec = m_DefaultJsonCodec;
+        }
+
+        public HttpMessage(string msgContent)
+            : this()
+        {
+            MessageType = MSG_TYPE_STRING;
+            MessageContent = msgContent;
+
+            ProtocolVersion = "HTTP/1.0";
+        }
+
+        public HttpMessage(string reqUrl, string reqMethod, string msgContent)
+            : this(msgContent)
+        {
+            RequestUrl = reqUrl;
+            RequestMethod = reqMethod;
+        }
+
+        public HttpMessage(int statusCode, string reasonPhrase, string msgContent)
+            : this(msgContent)
+        {
+            StatusCode = statusCode;
+            ReasonPhrase = reasonPhrase;
+        }
+
+        public HttpMessage(byte[] msgContent)
+            : this()
+        {
+            MessageType = MSG_TYPE_BINARY;
+            RawContent = msgContent;
+            ContentSize = msgContent.Length;
+
+            ProtocolVersion = "HTTP/1.0";
+        }
+
+        public HttpMessage(byte[] msgContent, int msgSize)
+            : this(msgContent)
+        {
+            ContentSize = msgSize;
+        }
+
+        public HttpMessage(int statusCode, string reasonPhrase, byte[] msgContent)
+            : this(msgContent)
+        {
+            StatusCode = statusCode;
+            ReasonPhrase = reasonPhrase;
+        }
+
+        public bool IsString()
+        {
+            return MessageType == MSG_TYPE_STRING;
+        }
+
+        public bool IsBinary()
+        {
+            return MessageType == MSG_TYPE_BINARY;
+        }
+
+        public void SetHeaders(Dictionary<string, string> headers)
+        {
+            Headers = new Dictionary<string, string>(headers);
+        }
+
+        public static Stack<Object> GetSessionBuffer(Session session, bool needCheck = false)
+        {
+            Stack<Object> result = null;
+            Dictionary<int, object> attrMap = session.GetAttributes();
+
+            if (needCheck)
+            {
+                lock (attrMap)
+                {
+                    if (attrMap.ContainsKey(WEB_MSG_BUF_CODE))
+                    {
+                        result = attrMap[WEB_MSG_BUF_CODE] as Stack<Object>;
+                        if (result == null) attrMap.Remove(WEB_MSG_BUF_CODE);
+                    }
+                    if (result == null)
+                    {
+                        result = new Stack<Object>();
+                        attrMap.Add(WEB_MSG_BUF_CODE, result);
+                    }
+                }
+            }
+            else
+            {
+                result = attrMap[WEB_MSG_BUF_CODE] as Stack<Object>;
+            }
+
+            return result;
+        }
+
+        public static Dictionary<string, object> GetSessionData(Session session, bool needCheck = false)
+        {
+            Dictionary<string, object> result = null;
+            Dictionary<int, object> attrMap = session.GetAttributes();
+
+            if (needCheck)
+            {
+                lock (attrMap)
+                {
+                    if (attrMap.ContainsKey(WEB_MSG_DATA_CODE))
+                    {
+                        result = attrMap[WEB_MSG_DATA_CODE] as Dictionary<string, object>;
+                        if (result == null) attrMap.Remove(WEB_MSG_DATA_CODE);
+                    }
+                    if (result == null)
+                    {
+                        result = new Dictionary<string, object>();
+                        attrMap.Add(WEB_MSG_DATA_CODE, result);
+                    }
+                }
+            }
+            else
+            {
+                result = attrMap[WEB_MSG_DATA_CODE] as Dictionary<string, object>;
+            }
+
+            return result;
+        }
+
+        public static void SetSessionData(Session session, string dataName, object dataValue)
+        {
+            Dictionary<string, object> dataMap = GetSessionData(session);
+            if (dataMap.ContainsKey(dataName)) dataMap.Remove(dataName);
+            dataMap.Add(dataName, dataValue);
+        }
+
+        public static object GetSessionData(Session session, string dataName)
+        {
+            Dictionary<string, object> dataMap = GetSessionData(session);
+            if (dataMap.ContainsKey(dataName)) return dataMap[dataName];
+            else return null;
+        }
+
+        public static TaskFactory GetSingleTaskFactory(Session session, bool needCheck = false)
+        {
+            TaskFactory result = null;
+            Dictionary<int, object> attrMap = session.GetAttributes();
+
+            if (needCheck)
+            {
+                lock (attrMap)
+                {
+                    if (attrMap.ContainsKey(WEB_MSG_TASK_CODE))
+                    {
+                        result = attrMap[WEB_MSG_TASK_CODE] as TaskFactory;
+                        if (result == null) attrMap.Remove(WEB_MSG_TASK_CODE);
+                    }
+                    if (result == null)
+                    {
+                        // this one-thread limitation could make sure that messages can be processed in correct order
+                        //result = new TaskFactory(new LimitedConcurrencyLevelTaskScheduler(1));
+                        result = new TaskFactory(m_TaskSchedulerPair.ExclusiveScheduler);
+                        attrMap.Add(WEB_MSG_TASK_CODE, result);
+                    }
+                }
+            }
+            else
+            {
+                result = attrMap[WEB_MSG_TASK_CODE] as TaskFactory;
+            }
+
+            return result;
+        }
+
+        public void FromJsonObject<T>(T obj) where T : class
+        {
+            var msgContent = m_JsonCodec.ToJsonString(obj);
+            MessageType = MSG_TYPE_STRING;
+            MessageContent = msgContent;
+            ProtocolVersion = "HTTP/1.0";
+        }
+
+        public T ToJsonObject<T>() where T : class
+        {
+            try
+            {
+                if (MessageContent.Length > 0)
+                {
+                    return m_JsonCodec.ToJsonObject<T>(MessageContent);
+                }
+                else
+                {
+                    return default(T);
+                }
+            }
+            catch
+            {
+                return default(T);
+            }
+        }
+
+        public static U ToJsonObject<U>(String str) where U : class
+        {
+            try
+            {
+                if (str.Length > 0)
+                {
+                    var codec = m_CurrentJsonCodec == null ? m_DefaultJsonCodec : m_CurrentJsonCodec;
+                    return codec.ToJsonObject<U>(str);
+                }
+                else
+                {
+                    return default(U);
+                }
+            }
+            catch
+            {
+                return default(U);
+            }
+        }
+
+        public static String ToJsonString<U>(U obj) where U : class
+        {
+            string str = "";
+            try
+            {
+                var codec = m_CurrentJsonCodec == null ? m_DefaultJsonCodec : m_CurrentJsonCodec;
+                str = codec.ToJsonString(obj);
+            }
+            catch { }
+            return str;
+        }
+
+        
+    }
+
+    public interface IJsonCodec
+    {
+        string ToJsonString(object obj);
+        T ToJsonObject<T>(string str) where T : class;
+    }
+
+    public class SimpleJsonCodec : IJsonCodec
+    {
+        public string ToJsonString(object obj)
+        {
+            DataContractJsonSerializer serializer = new DataContractJsonSerializer(obj.GetType());
+            using (MemoryStream ms = new MemoryStream())
+            {
+                serializer.WriteObject(ms, obj);
+                return Encoding.UTF8.GetString(ms.ToArray());
+            }
+        }
+
+        public T ToJsonObject<T>(string str) where T : class
+        {
+            if (str == null || str.Length <= 0) return null;
+            else
+            {
+                DataContractJsonSerializer serializer = new DataContractJsonSerializer(typeof(T));
+                using (MemoryStream ms = new MemoryStream(Encoding.UTF8.GetBytes(str)))
+                {
+                    return serializer.ReadObject(ms) as T;
+                }
+            }
+        }
+
+    }
+}
---
+++ net45/SharpNetwork.SimpleHttp/NetworkEventHandler.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using SharpNetwork.Core;
+
+namespace SharpNetwork.SimpleHttp
+{
+    public class NetworkEventHandler : CommonNetworkEventHandler
+    {
+        HttpRouter m_MessageHandlerManager = null;
+
+        public bool AllowOrderlyProcess { get; set; }
+
+        public NetworkEventHandler(HttpRouter messageHandlerManager) : base()
+        {
+            m_MessageHandlerManager = messageHandlerManager;
+            AllowOrderlyProcess = false;
+        }
+
+        public NetworkEventHandler(HttpRouter messageHandlerManager, NetworkEventPackage events)
+            : base(events)
+        {
+            m_MessageHandlerManager = messageHandlerManager;
+            AllowOrderlyProcess = false;
+        }
+
+        public HttpRouter GetHandlerManager()
+        {
+            return m_MessageHandlerManager;
+        }
+
+        protected virtual void ProcessMessage(Object task)
+        {
+            Session session = null;
+            try
+            {
+                if (m_MessageHandlerManager == null || task == null) return;
+
+                SessionContext ctx = (SessionContext)task;
+                session = ctx.Session;
+                HttpMessage msg = (HttpMessage)ctx.Data;
+
+                IHandler handler = m_MessageHandlerManager.GetHandler(msg.RequestUrl);
+                if (handler == null) return;
+
+                // you may run some complex decode function here ...
+                //MessageCodec.DecodeMessage(session, msg);
+
+                handler.Handle(ctx);
+            }
+            catch (Exception ex)
+            {
+                try
+                {
+                    OnError(session, Session.ERROR_RECEIVE, ex.Message);
+                }
+                catch { }
+            }
+
+        }
+
+        public override void OnConnect(Session session)
+        {
+            HttpMessage.GetSessionBuffer(session, true);
+            HttpMessage.GetSessionData(session, true);
+
+            // one factory for one session
+            if (AllowOrderlyProcess) HttpMessage.GetSingleTaskFactory(session, true);
+
+            base.OnConnect(session);
+        }
+
+        public override int OnReceive(Session session, Object data)
+        {
+            SessionContext ctx = new SessionContext(session, data);
+            HttpMessage msg = (HttpMessage)ctx.Data;
+
+            // run further decode process here (or may run it within the thread, see ProcessMessage())
+            //MessageCodec.DecodeMessage(session, msg);
+
+            TaskFactory factory = Task.Factory;
+
+            if (AllowOrderlyProcess && ctx.Session != null)
+                factory = HttpMessage.GetSingleTaskFactory(ctx.Session);
+
+            if (factory != null)
+            {
+                factory.StartNew((Action<object>)((param) =>
+                {
+                    ProcessMessage(param);
+                }), ctx);
+            }
+            else ProcessMessage(ctx);
+
+            return base.OnReceive(session, data);
+        }
+
+    }
+}
